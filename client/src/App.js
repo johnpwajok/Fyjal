69c6e1ef411ab5b6566f2ee1747ab8b25f09014f
John Pwajok <johnpwajok97@hotmail.com>
2020-09-03 16:10:23 +0100

    Base files created and working

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavigationBar from "./components/NavigationBar";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "./components/Landing";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.connectToServer = this.connectToServer.bind(this);
+  }
+  connectToServer() {
+    fetch("/");
+  }
+  componentDidMount() {
+    this.connectToServer();
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <NavigationBar />
+          <Route exact path="/" component={Landing} />
+          <div className="container"></div>
+        </div>
+      </Router>
+    );
+  }
 }
-
 export default App;
